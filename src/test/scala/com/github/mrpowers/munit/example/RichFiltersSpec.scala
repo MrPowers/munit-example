048d39a8fab65648d276167c14a26e34da20a2ce
MrPowers <matthewkevinpowers@gmail.com>
2020-03-08 11:47:44 -0500

    Try to get rich filters working

--- src/test/scala/com/github/mrpowers/munit/example/RichFiltersSpec.scala
+++ src/test/scala/com/github/mrpowers/munit/example/RichFiltersSpec.scala
@@ -1,26 +1,37 @@
-//package com.github.mrpowers.munit.example
-//
-//import scala.util.Properties
-//import munit._
-//
-//object Windows213 extends Tag("Windows213")
-//
-//class MySuite extends FunSuite {
-//  // reminder: type Test = GenericTest[Any]
-//  override def munitNewTest(test: Test): Test = {
-//    val isIgnored =
-//      test.tags(Windows213) && !(
-//        Properties.isWin &&
-//          Properties.versionNumberString.startsWith("2.13")
-//        )
-//    if (isIgnored) test.withBody(() => Ignore)
-//    else test
-//  }
-//
-//  test("windows-213".tag(Windows213)) {
-//    // Only runs when operating system is Windows and Scala version is 2.13
-//  }
-//  test("normal test") {
-//    // Always runs like a normal test.
-//  }
-//}
+package com.github.mrpowers.munit.example
+
+import scala.util.Properties
+import munit._
+
+object Windows213 extends Tag("Windows213")
+
+class MySuite extends FunSuite {
+
+  override def munitNewTest(test: Test): Test = {
+    println("test.tags(Windows213): " + test.tags(Windows213))
+    val isIgnored =
+      test.tags(Windows213) && !(
+        Properties.isWin &&
+          Properties.versionNumberString.startsWith("2.13")
+        )
+    if (isIgnored) test.tag(Ignore)
+    else test
+  }
+
+  test("windows-213".tag(Windows213)) {
+    println("isWin: " + Properties.isWin)
+    println("""Properties.versionNumberString.startsWith("2.13"): """ + Properties.versionNumberString.startsWith("2.13"))
+    println("Only runs when operating system is Windows and Scala version is 2.13")
+    assertEquals(2, 3)
+  }
+
+  // true && !(false && false)
+  // true && !(false)
+  // true && true
+  // true
+
+  test("normal test") {
+    assertEquals(2, 2)
+  }
+
+}
